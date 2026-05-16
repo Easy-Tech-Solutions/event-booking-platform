128d640e8e1c5e79acae306e46a8ef75ce43ea5c
jdaltonll02 <dalton.edu02@gmail.com>
2026-05-16 22:49:41 +0200

    fixing signup issue

--- backend/src/config/stripe.js
+++ backend/src/config/stripe.js
@@ -5,6 +5,7 @@ import env from './env.js';
 const { STRIPE_SECRET_KEY } = env;
 
 if (!STRIPE_SECRET_KEY) {
+  console.error('Startup error: STRIPE_SECRET_KEY is required but missing.');
   throw new Error('STRIPE_SECRET_KEY is required');
 }
 
